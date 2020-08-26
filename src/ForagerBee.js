a031ec093d8a2ad06ae0d6631fd50ecb626c544f
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-08-26 00:11:51 -0700

    Complete ES6 Instanciation of HoneyMakerBee and ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,29 @@
-class ForagerBee {
+class ForagerBee extends Bee {
   // TODO..
+  constructor() {
+    super() //call superclass
+
+    //Overwrite properties from superclass
+    this.age = 10;
+    this.job = 'find pollen';
+
+    //inherit from superclass
+    //this.color = 'yellow';
+    //this.food = 'jelly';
+
+    //new methods
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  //Inherited methods
+  /*
+  eat() {
+    ...
+  } */
+
+  //New methods
+  forage(treasure) {
+    return this.treasureChest.push(treasure);
+  }
 };
