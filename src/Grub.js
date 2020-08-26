867f440e74af34406167a9c5166ffac5fc88dd86
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-08-25 23:50:43 -0700

    Complete ES6 Instanciation of Grub.js

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,17 @@
 class Grub {
+
+//From MDN:
+/* Arrow function expressions are a syntactically compact alternative to writing regular function expressions, althought WITHOUT its own bindings to THIS, ARGUMENTS, SUPER or NEW.TARGET keywords.
+
+Arrow functions are ill-suited as methods and they CANNOT be used as constructors.*/
+
+constructor() {
+this.age = 0;
+this.color = 'pink';
+this.food = 'jelly';
+}
+
+eat() {
+  return 'Mmmmmmmmm jelly';
+}
 };
